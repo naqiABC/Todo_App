f92624669077bc67b2d268bf46d21cdb49423f56
naqiABC <syednaqi4555@gmail.com>
2023-11-02 13:30:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react"; 
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+    <div className="App">
+   <h1>TODO APP</h1>
+   </div>
+   <div className="input">
+  <input type="text" />
+ 
+  <div className="Addbtn">
+  <button >Add</button>
+  </div>
+  </div>
+    </div>
+    
+
+
+  );
+}
+
+export default App;
