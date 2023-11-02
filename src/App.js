c5ca795a3258b294ab4f0f03ac5b8b5643b5537b
naqiABC <syednaqi4555@gmail.com>
2023-11-02 16:30:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from "react"; 
-
+import React from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="container">
-    <div className="App">
-   <h1>TODO APP</h1>
-   </div>
-   <div className="input">
-  <input type="text" />
- 
-  <div className="Addbtn">
-  <button >Add</button>
-  </div>
-  </div>
+    <div className='todo-app'>
+      <TodoList />
     </div>
-    
-
-
   );
 }
 
